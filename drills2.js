a8ec6d4260798488040febeaf24adfbd996cd78a
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-08 14:36:51 -0400

    p2

--- drills2.js
+++ drills2.js
@@ -5,4 +5,22 @@ function jediName(firstName, lastName){
   return jediName;
 }
 
+function beyond (num) {
+  if (num === Infinity) {
+    console.log('And beyond');
+  } else if (num > 0) {
+    console.log('To infinity');
+  } else if (num < 0) {
+    console.log('To negative infinity');
+  } else if (num === 0) {
+    console.log('Staying home');
+  } else {
+    console.log('input invalid');
+  }
+}
+
 console.log(jediName('cody', 'oberholtzer'));
+beyond(Infinity);
+beyond(10);
+beyond(-10);
+beyond(0);
