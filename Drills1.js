2c508c88dd7e39250d21101f5e7b6aa9175bba03
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-08 14:01:29 -0400

    step 5

--- Drills1.js
+++ Drills1.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function createGreeting(name, age) {
-  return `Hi, I'm ${name} and I'm ${age} years old`;
+  let yearOfBirth = 2019-age;
+  return `Hi, I'm ${name} and I'm ${age} years old. I was born in the year ${yearOfBirth}`;
 }
 
 let greeting = createGreeting('lars', 99);
