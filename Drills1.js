812424b5f0d860c03f57054eb951e3f035055a57
krill <lpmaze@gmail.com>
2019-07-08 14:03:06 -0400

    step 6

--- Drills1.js
+++ Drills1.js
@@ -1,8 +1,12 @@
 'use strict';
 
 function createGreeting(name, age) {
-  let yearOfBirth = 2019-age;
+  let yearOfBirth = getYearOfBirth(age);
   return `Hi, I'm ${name} and I'm ${age} years old. I was born in the year ${yearOfBirth}`;
+}
+
+function getYearOfBirth(age){
+  return 2019-age;
 }
 
 let greeting = createGreeting('lars', 99);
