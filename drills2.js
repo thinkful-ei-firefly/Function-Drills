399e54433d9ca476bc9d3a4cdb9137520449e7e9
krill <lpmaze@gmail.com>
2019-07-08 14:49:02 -0400

    added decode() function

--- drills2.js
+++ drills2.js
@@ -1,11 +1,11 @@
 'use strict';
 
-function jediName(firstName, lastName){
-  let jediName = lastName.slice(0,3) + firstName.slice(0,2);
+function jediName(firstName, lastName) {
+  let jediName = lastName.slice(0, 3) + firstName.slice(0, 2);
   return jediName;
 }
 
-function beyond (num) {
+function beyond(num) {
   if (num === Infinity) {
     console.log('And beyond');
   } else if (num > 0) {
@@ -19,8 +19,31 @@ function beyond (num) {
   }
 }
 
+function decode(word) {
+  let firstLetter = word.charAt(0);
+  let letter = ' ';
+  switch (firstLetter) {
+  case 'a':
+    letter = word.charAt(1);
+    break;
+  case 'b':
+    letter = word.charAt(2);
+    break;
+  case 'c':
+    letter = word.charAt(3);
+    break;
+  case 'd':
+    letter = word.charAt(4);
+    break;
+  }
+
+  return letter;
+}
+
 console.log(jediName('cody', 'oberholtzer'));
 beyond(Infinity);
 beyond(10);
 beyond(-10);
 beyond(0);
+console.log(decode('hello'));
+console.log(decode('alpha'));
